ec02523a18e49a6c880944b8ad7e89dea7b062d8
iceberg12 <iceberg12@users.noreply.github.com>
2016-02-22 10:40:56 +0800

    Update chisquare.scala

--- DistributedChisquare/chisquare.scala
+++ DistributedChisquare/chisquare.scala
@@ -1,6 +1,7 @@
 /**
  * Created by mikenguyen on 1/29/2016.
  */
+// databases and variables have been masked
 
 import org.apache.spark.{sql, SparkConf, SparkContext}
 import org.apache.hadoop.security.UserGroupInformation
@@ -79,48 +80,44 @@ object Main {
       // "abc".getBytes.mkString.toDouble
     })
 
-    val sqlStr = """SELECT DISTINCT SV.wis_ooc, cast(SC.ch_id as String) ch_id, SLW.exval_01 PartType, SLW.exval_06 as process_step_name, SLW.parameter_name
-            , cast(SLW.sample_id as String) sample_id, SLW.exval_12 as lot_id, regexp_replace(SLW.exval_12, '\\.0\\p{Alnum}', '\\.00') as parent_lot
-            , SLW.sample_date, SR.raw_value
-            , FLH.step_name, FLEH.equip_id
-      FROM (SELECT sample_id, parameter_name, sample_date, exval_01, exval_06, exval_12, month FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_WAFER a
+    val sqlStr = """SELECT DISTINCT SV.wo, cast(SC.ch as String) ch, SLW.e_01 pt, SLW.e_06 as step, SLW.pr
+            , cast(SLW.sample as String) sample, SLW.e_12 as lid, regexp_replace(SLW.e_12, '\\.0\\p{Alnum}', '\\.00') as parent
+            , SLW.sd, SR.raw_value
+            , FLH.sn, FLEH.ei
+      FROM (SELECT sample, pr, sd, e_01, e_06, e_12, month FROM data.sw a
             WHERE (month = 'm_0' or month = 'm_70')
-            AND sample_date > cast('last7day' as timestamp)
+            AND sd > cast('last7day' as timestamp)
             UNION ALL
-            SELECT sample_id, parameter_name, sample_date, exval_01, exval_06, exval_12, month FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_LOT b
+            SELECT sample, pr, sd, e_01, e_06, e_12, month FROM data.sl b
             WHERE (month = 'm_0' or month = 'm_70')
-            AND sample_date > cast('last7day' as timestamp)
+            AND sd > cast('last7day' as timestamp)
             ) SLW
-      INNER JOIN (SELECT sample_id, raw_value, month FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_RAW_WAFER
+      INNER JOIN (SELECT sample, raw_value, month FROM data.sr1
             WHERE (month = 'm_0' or month = 'm_70')
             UNION ALL
-            SELECT sample_id, max(raw_value) as raw_value, month FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_RAW_LOT
+            SELECT sample, max(raw_value) as raw_value, month FROM data.sr2
             WHERE (month = 'm_0' or month = 'm_70')
-            GROUP BY sample_id, month
+            GROUP BY sample, month
             ) SR
-        ON SR.sample_id = SLW.sample_id
-      INNER JOIN (SELECT ch_id, sample_id, out_of_order, ckc_id, month FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_CALC
+        ON SR.sample = SLW.sample
+      INNER JOIN (SELECT ch, sample, out_of_order, ckc_id, month FROM data.sc
             WHERE month = 'm_0' or month = 'm_70'
             AND out_of_order = 'N'
             AND ckc_id = 0) SC
-        ON SC.sample_id = SLW.sample_id
-      INNER JOIN (SELECT * FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_T_CHANNEL_DEF
-                  WHERE cf_value_11 IN ('PHOTO')) TCD
-        ON SC.ch_id = TCD.ch_id
-      LEFT JOIN (SELECT DISTINCT sample_date,ch_id as sv_ch_id, sample_id,'1' AS wis_ooc,month
-            FROM PROD_MTI_SINGAPORE_FAB_7_SPACE.IDL_SAMPLES_VIOL) SV
-        ON (SLW.sample_id = SV.sample_id and SV.sv_ch_id = SC.ch_id)
-      INNER JOIN PROD_MTI_SINGAPORE_FAB_7_FABTRKG.IDL_FAB_LOT_HISTORY FLH
+        ON SC.sample = SLW.sample
+      LEFT JOIN (SELECT DISTINCT sd,ch as sv_ch, sample,'1' AS wo,month
+            FROM data.isv) SV
+        ON (SLW.sample = SV.sample and SV.sv_ch = SC.ch)
+      INNER JOIN ftrkg.flhy FLH
         ON ((FLH.month='m_0' and FLH.year = 'y_0') or (FLH.month='m_1' and FLH.year = 'y_1') or (FLH.month='m_2' AND FLH.year = 'y_2'))
-        AND FLH.lot_id = regexp_replace(SLW.exval_12, '\\.0\\p{Alnum}', '\\.00')
-        AND FLH.tracked_out_datetime > date_sub(SLW.sample_date, 4)
-        AND FLH.tracked_out_datetime < SLW.sample_date
-        AND FLH.step_name not like '1%'
-      INNER JOIN PROD_MTI_SINGAPORE_FAB_7_FABTRKG.IDL_FAB_LOT_EQUIP_HISTORY FLEH
+        AND FLH.lid = regexp_replace(SLW.e_12, '\\.0\\p{Alnum}', '\\.00')
+        AND FLH.tro_datetime > date_sub(SLW.sd, 4)
+        AND FLH.tro_datetime < SLW.sd
+        AND FLH.sn not like '1%'
+      INNER JOIN ftrkg.flehy FLEH
         ON ((FLEH.month='m_0' and FLEH.year = 'y_0') or (FLEH.month='m_1' and FLEH.year = 'y_1') or (FLEH.month='m_2' AND FLEH.year = 'y_2'))
-        AND FLH.fab_lot_hist_oid = FLEH.fab_lot_hist_oid
-      WHERE SLW.parameter_name IN ('TOTAL_DEFECTIVE_DIE_BLANKET', 'TOTAL_DEFECTS_BLANKET', 'INSPECTION_FAILED_BLANKET')
-      ORDER BY ch_id, SLW.sample_date"""
+        AND FLH.flho = FLEH.flho
+      ORDER BY ch, SLW.sd"""
 
     val sqlStr1 = (sqlStr
       .replaceAll("last21day", last21day).replaceAll("last7day", last7day).replaceAll("today", today)
@@ -132,13 +129,13 @@ object Main {
     df.cache()
 
     // New method
-    def toolEnd00 = udf((s1: String) => {
+    def tEnd00 = udf((s1: String) => {
       s1.endsWith("00")
     })
-    val df1 = df.filter(toolEnd00(df("equip_id"))).
-      select(getConcatenated3(df("wis_ooc"), df("ch_id"), df("sample_id") as "key")
-        , df("step_name")
-        , strToIntstr(df("equip_id")) as "equip_id").
+    val df1 = df.filter(tEnd00(df("ei"))).
+      select(getConcatenated3(df("wo"), df("ch"), df("sample") as "key")
+        , df("sn")
+        , strToIntstr(df("ei")) as "ei").
       rdd
     val pivot = df1.keyBy(r => r(0).toString).aggregateByKey(Map[String, String]())(
       (map1, row) => map1 + (row(1).toString -> row(2).toString),
@@ -152,29 +149,29 @@ object Main {
         (k.split("\\|") ++ columns.map(c => if (map.contains(c)) map(c).toDouble else 0.0))
           .toSeq)
     }
-    val schema1 = StructType(Array(StructField("wis_ooc", StringType), StructField("ch_id", StringType), StructField("sample_id", StringType))
+    val schema1 = StructType(Array(StructField("wo", StringType), StructField("ch", StringType), StructField("sample", StringType))
       ++ columns.map(c => StructField(c, DoubleType)))
     val fin = sqlContext.createDataFrame(result1, schema1)
     fin.cache()
-    val df_trav = fin.withColumn("label", wisToDouble1(fin("wis_ooc")))
+    val df_trav = fin.withColumn("label", wisToDouble1(fin("wo")))
 
     // select channel ids with at least 3 OOCs
     val a4 = df_trav.
-      select(df_trav("ch_id"), df_trav("wis_ooc")).
-      filter(df_trav("wis_ooc") === 1.0).groupBy("ch_id").count
-    val ch_ids = a4.filter(a4("count") >= 3).
+      select(df_trav("ch"), df_trav("wo")).
+      filter(df_trav("wo") === 1.0).groupBy("ch").count
+    val chs = a4.filter(a4("count") >= 3).
       map(r => r(0).toString).distinct.collect()
 
-    val ignored = List("wis_ooc", "ch_id", "sample_id", "label")
+    val ignored = List("wo", "ch", "sample", "label")
     val columns1 = df_trav.columns
     val featInd = columns1.diff(ignored).map(columns1.indexOf(_))
     val ncol = featInd.length
     val targetInd = columns1.indexOf("label")
     val featureName = featInd.map(columns1(_))
 
-    val imp_travelStep = ch_ids.map { k =>
-      val a1 = df_trav.filter(df_trav("ch_id") === k)
-      val a2 = a1.count() - a1.filter(a1("wis_ooc") === 1.0).count()
+    val imp_travelStep = chs.map { k =>
+      val a1 = df_trav.filter(df_trav("ch") === k)
+      val a2 = a1.count() - a1.filter(a1("wo") === 1.0).count()
 
       if (a2 >= 3) {
         val b1 = a1.map { r =>
@@ -198,80 +195,80 @@ object Main {
     impChanTrav.length
     //fin.unpersist()
 
-    // After getting affecting traveler steps, highlight problematic tools
+    // After getting affecting traveler steps, highlight problematic ts
     def keyContain = udf((str: String) => {
       impChanTrav.contains(str)
     })
-    val df2 = df.select(getConcatenated2(df("ch_id"), df("FLH.step_name")) as "key"
-      , df("sample_id"), df("equip_id")
-      , wisToDouble(df("wis_ooc")) as "wis_ooc")
+    val df2 = df.select(getConcatenated2(df("ch"), df("FLH.sn")) as "key"
+      , df("sample"), df("ei")
+      , wisToDouble(df("wo")) as "wo")
 
-    // find major tools contributing to OOC
-    val df3_1 = df2.filter(keyContain(df2("key"))).filter(df2("wis_ooc") === 1.0)
+    // find major ts contributing to OOC
+    val df3_1 = df2.filter(keyContain(df2("key"))).filter(df2("wo") === 1.0)
     val df4_1 = df3_1.
       groupBy(df3_1("key")).
-      agg(df3_1("key"), countDistinct(df3_1("sample_id")) as "total_1")
+      agg(df3_1("key"), countDistinct(df3_1("sample")) as "total_1")
     val a5 = df3_1.
-      select(df3_1("key") as "key1", df3_1("sample_id") as "sample_id1", df3_1("equip_id") as "equip_id1")
+      select(df3_1("key") as "key1", df3_1("sample") as "sample1", df3_1("ei") as "ei1")
     val df5_1 = a5.
-      groupBy(a5("key1"), a5("equip_id1")).
-      agg(a5("key1"), a5("equip_id1"), countDistinct(a5("sample_id1")) as "no_1_per_tool")
+      groupBy(a5("key1"), a5("ei1")).
+      agg(a5("key1"), a5("ei1"), countDistinct(a5("sample1")) as "no_1_per_t")
     val divide = udf((x: Long, y: Long) => {
       x.toDouble / y
     })
     val a1 = df4_1.join(df5_1, df4_1("key") === df5_1("key1"))
     val df_1 = a1.
       filter(a1("total_1") >= 3).
-      select(a1("key"), a1("equip_id1") as "equip_id"
-        , divide(a1("no_1_per_tool"), a1("total_1")) as "perct_1")
+      select(a1("key"), a1("ei1") as "ei"
+        , divide(a1("no_1_per_t"), a1("total_1")) as "perct_1")
     df_1.cache()
 
-    val df3_2 = df2.filter(keyContain(df2("key"))).filter(df2("wis_ooc") === 0.0)
+    val df3_2 = df2.filter(keyContain(df2("key"))).filter(df2("wo") === 0.0)
     val df4_2 = df3_2.
       groupBy(df3_2("key")).
-      agg(df3_2("key"), countDistinct(df3_2("sample_id")) as "total_0")
+      agg(df3_2("key"), countDistinct(df3_2("sample")) as "total_0")
     val a6 = df3_2.
-      select(df3_2("key") as "key1", df3_2("sample_id") as "sample_id1", df3_2("equip_id") as "equip_id1")
+      select(df3_2("key") as "key1", df3_2("sample") as "sample1", df3_2("ei") as "ei1")
     val df5_2 = a6.
-      groupBy(a6("key1"), a6("equip_id1")).
-      agg(a6("key1"), a6("equip_id1"), countDistinct(a6("sample_id1")) as "no_0_per_tool")
+      groupBy(a6("key1"), a6("ei1")).
+      agg(a6("key1"), a6("ei1"), countDistinct(a6("sample1")) as "no_0_per_t")
 
     val a2 = df4_2.join(df5_2, df4_2("key") === df5_2("key1"))
     val df_0 = a2.
       filter(a2("total_0") >= 3).
-      select(a2("key") as "key0", a2("equip_id1") as "equip_id0"
-        , divide(a2("no_0_per_tool"), a2("total_0")) as "perct_0")
+      select(a2("key") as "key0", a2("ei1") as "ei0"
+        , divide(a2("no_0_per_t"), a2("total_0")) as "perct_0")
     df_0.cache()
 
-    // outer join to get all tools the process either wis 0.0 and 1.0
+    // outer join to get all ts the process either wis 0.0 and 1.0
     val classify = udf((x: Double, y: Double) => {
       if (x > 0.8) {
         if (y > 0.8) {
-          "Major_tool"
+          "Major_t"
         } else if (x / y > 2) {
-          "Bad_tool"
+          "Bad_t"
         } else {
           "Nothing"
         }
       }
       else if (x > 0.3 && x / y > 2) {
-        "Bad_tool"
+        "Bad_t"
       }
       else if (x < 0.3 && x / y > 3) {
-        "Bad_tool"
+        "Bad_t"
       }
       else {
         "Nothing"
       }
     })
-    val a3 = df_1.join(df_0, df_1("key") === df_0("key0") and df_1("equip_id") === df_0("equip_id0"), "left_outer")
+    val a3 = df_1.join(df_0, df_1("key") === df_0("key0") and df_1("ei") === df_0("ei0"), "left_outer")
     val a7 = a3.
-      select(a3("key"), a3("equip_id"), a3("perct_1"), a3("perct_0"),
-        classify(a3("perct_1"), a3("perct_0")) as "tool_classified")
-    val df_tool = a7.
-      filter(a7("tool_classified") === "Major_tool" or a7("tool_classified") === "Bad_tool").
-      orderBy(a7("key"), a7("equip_id"))
-    df_tool.cache()
+      select(a3("key"), a3("ei"), a3("perct_1"), a3("perct_0"),
+        classify(a3("perct_1"), a3("perct_0")) as "t_classified")
+    val df_t = a7.
+      filter(a7("t_classified") === "Major_t" or a7("t_classified") === "Bad_t").
+      orderBy(a7("key"), a7("ei"))
+    df_t.cache()
 
     // *******************************************************
     // Output
@@ -282,50 +279,50 @@ object Main {
     val getStep = udf((s1: String) => {
       s1.split("\\|")(1)
     })
-    val m3 = df_tool.select(getChId(df_tool("key")) as "channel_id", getStep(df_tool("key")) as "step"
-      , df_tool("equip_id") as "tool_name", df_tool("perct_1"), df_tool("perct_0"), df_tool("tool_classified"))
+    val m3 = df_t.select(getChId(df_t("key")) as "cid", getStep(df_t("key")) as "step"
+      , df_t("ei") as "tn", df_t("perct_1"), df_t("perct_0"), df_t("t_classified"))
     /*val m2 = m1.map(a => Row(a(0), a(1), a(2)))
-    val schema = StructType(Array(StructField("channel_id", StringType),
+    val schema = StructType(Array(StructField("cid", StringType),
       StructField("step", StringType),
-      StructField("tool_name", StringType)))
+      StructField("tn", StringType)))
     val m3 = sqlContext.createDataFrame(m2, schema)*/
 
     sqlContext.sql("use user_mikenguyen")
     // write analysis
     val sqlStr2 =
-      """CREATE TABLE IF NOT EXISTS user_mikenguyen.tb_wis_ooc_analysis_fast
-        |(ch_id string, past_traveler_step string, tool string, perct_1 double
-        |, perct_0 double, tool_classified string)
+      """CREATE TABLE IF NOT EXISTS user_mikenguyen.two
+        |(ch string, past_ts string, t string, perct_1 double
+        |, perct_0 double, t_classified string)
         |ROW FORMAT DELIMITED
         |FIELDS TERMINATED BY '\t'
       """.stripMargin
     sqlContext.sql(sqlStr2)
-    m3.insertInto("tb_wis_ooc_analysis_fast", true)
+    m3.insertInto("two", true)
 
     // write combined result
     val sqlStr3 =
-      """CREATE TABLE IF NOT EXISTS user_mikenguyen.tb_wis_ooc_result_fast
-        |(wis_ooc string, ch_id string, part_type string, process_step_name string, parameter_name string, sample_id string
-        |, lot_id string, parent_lot string, sample_date timestamp, raw_value double
-        |, past_step_name string, equip_name string
-        |, perct_1 double, perct_0 double, tool_classified string)
+      """CREATE TABLE IF NOT EXISTS user_mikenguyen.rlt
+        |(wo string, ch string, part_type string, step string, pr string, sample string
+        |, lid string, parent string, sd timestamp, raw_value double
+        |, past_sn string, equip_name string
+        |, perct_1 double, perct_0 double, t_classified string)
         |ROW FORMAT DELIMITED
         |FIELDS TERMINATED BY '\t'
       """.stripMargin
     sqlContext.sql(sqlStr3)
-    val m4 = m3.select("channel_id").distinct
-    val df1_2 = df.join(m4, df("ch_id") === m4("channel_id"))
+    val m4 = m3.select("cid").distinct
+    val df1_2 = df.join(m4, df("ch") === m4("cid"))
     val df2_2 = df1_2.
-      select("wis_ooc", "ch_id", "PartType", "process_step_name", "parameter_name", "sample_id",
-        "lot_id", "parent_lot", "sample_date", "raw_value", "step_name", "equip_id").
-      join(m3, df1_2("ch_id") === m3("channel_id") and df1_2("step_name") === m3("step")
-        and df1_2("equip_id") === m3("tool_name"), "left_outer")
-    /*val df1_2 = df.join(m3, $"ch_id" === $"channel_id" &
-      $"step_name" === $"step" &
-      $"equip_id" === $"tool_name")*/
-    df2_2.select("wis_ooc", "ch_id", "PartType", "process_step_name", "parameter_name", "sample_id",
-      "lot_id", "parent_lot", "sample_date", "raw_value", "step_name", "equip_id",
-      "perct_1", "perct_0", "tool_classified").
-      insertInto("tb_wis_ooc_result_fast", true)
+      select("wo", "ch", "pt", "step", "pr", "sample",
+        "lid", "parent", "sd", "raw_value", "sn", "ei").
+      join(m3, df1_2("ch") === m3("cid") and df1_2("sn") === m3("step")
+        and df1_2("ei") === m3("tn"), "left_outer")
+    /*val df1_2 = df.join(m3, $"ch" === $"cid" &
+      $"sn" === $"step" &
+      $"ei" === $"tn")*/
+    df2_2.select("wo", "ch", "pt", "step", "pr", "sample",
+      "lid", "parent", "sd", "raw_value", "sn", "ei",
+      "perct_1", "perct_0", "t_classified").
+      insertInto("tb_wo_result_fast", true)
   }
 }
